1de061bbfcd8bd5858316465a75187515dca2db2
OX-S <finn.kliewer1@gmail.com>
2021-02-27 19:11:24 -0500

    Started Custom Enchants

---
+++ src/main/java/dev/arcticdevelopment/arctictools/commands/enchants/rods/SpawnerFinderEnchant.java
@@ -0,0 +1,23 @@
+package dev.arcticdevelopment.arctictools.commands.enchants.rods;
+
+import dev.arcticdevelopment.arctictools.commands.controllers.RodEnchant;
+
+
+
+public class SpawnerFinderEnchant extends RodEnchant {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public String getReferenceName() {
+		return null;
+	}
+
+	@Override
+	public int getMaxLevel() {
+		return 10;
+	}
+}
