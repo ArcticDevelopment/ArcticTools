0005f25336d70648d6820cee98dbe31d9469901c
KyroKrypt <kyrokrypt@gmail.com>
2021-03-01 13:45:27 -0800

    Set up loot editor to use base 64 encoding to save

--- src/main/java/dev/arcticdevelopment/arctictools/commands/AdminDropCommand.java
+++ src/main/java/dev/arcticdevelopment/arctictools/commands/AdminDropCommand.java
@@ -1,11 +1,8 @@
 package dev.arcticdevelopment.arctictools.commands;
 
-import dev.arcticdevelopment.arctictools.ArcticTools;
-import dev.arcticdevelopment.arctictools.utilities.Base64;
+import dev.arcticdevelopment.arctictools.inventories.LootEditor;
 import dev.kyro.arcticapi.commands.ASubCommand;
-import dev.kyro.arcticapi.data.AConfig;
 import dev.kyro.arcticapi.data.APlayerData;
-import dev.kyro.arcticapi.misc.AOutput;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -26,12 +23,6 @@ public class AdminDropCommand extends ASubCommand {
 		Player player = (Player) sender;
 		FileConfiguration playerData = APlayerData.getPlayerData(player);
 
-		if(player.getItemInHand() == null) {
-			AOutput.error(player, "You have to be holding an item in your hand");
-			return;
-		}
-
-		AConfig.set("loot", Base64.itemTo64(player.getItemInHand()));
-		ArcticTools.INSTANCE.saveConfig();
+		player.openInventory(new LootEditor().getInventory());
 	}
 }
--- src/main/java/dev/arcticdevelopment/arctictools/inventories/LootEditor.java
+++ src/main/java/dev/arcticdevelopment/arctictools/inventories/LootEditor.java
@@ -1,35 +1,85 @@
 package dev.arcticdevelopment.arctictools.inventories;
 
+import dev.arcticdevelopment.arctictools.ArcticTools;
+import dev.arcticdevelopment.arctictools.utilities.Base64;
+import dev.kyro.arcticapi.data.AConfig;
 import dev.kyro.arcticapi.gui.AInventoryGUI;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LootEditor extends AInventoryGUI {
 
-	public static List<ItemStack> drops = new ArrayList<>();
-
 	public LootEditor() {
-		super("Loot Editor", 6);
+		super("Loot Editor", 1);
 	}
 
 	@Override
 	public void onClick(InventoryClickEvent event) {
 
-		System.out.println(event.getClick());
+		Player player = (Player) event.getWhoClicked();
+
+		if(event.getCurrentItem() == null) return;
+		ItemStack clickedItem = event.getCurrentItem();
+
+		if(event.getClickedInventory() instanceof PlayerInventory) {
+
+			event.setCurrentItem(new ItemStack(Material.AIR));
+			event.getInventory().addItem(clickedItem);
+
+		} else {
+
+			for(int i = event.getSlot(); i < event.getInventory().getSize() - 1; i++) {
+
+				event.getInventory().setItem(i, event.getInventory().getItem(i + 1));
+			}
+			event.getInventory().setItem(event.getInventory().getSize() - 1, new ItemStack(Material.AIR));
+
+//			event.setCurrentItem(new ItemStack(Material.AIR));
+			player.getInventory().addItem(clickedItem);
+		}
 	}
 
 	@Override
 	public void onOpen(InventoryOpenEvent event) {
+
 
 	}
 
 	@Override
 	public void onClose(InventoryCloseEvent event) {
 
+		AConfig.set("rod-loot", new ArrayList<>());
+
+		for(ItemStack drop : event.getInventory().getContents()) {
+
+			if(drop == null) continue;
+			AConfig.addToList("rod-loot", Base64.itemTo64(drop));
+		}
+		ArcticTools.INSTANCE.saveConfig();
+	}
+
+	@Override
+	public Inventory getInventory() {
+
+		Inventory inventory = super.getInventory();
+		List<String> drops = AConfig.getStringList("rod-loot");
+
+		for(String drop : drops) {
+			try {
+				inventory.addItem(Base64.itemFrom64(drop));
+			} catch(IOException ignored) {}
+		}
+
+		return inventory;
 	}
 }
