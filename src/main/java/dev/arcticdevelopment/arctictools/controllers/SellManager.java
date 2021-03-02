0845bce6babecd9e422a327c826d78177eb7426d
KyroKrypt <kyrokrypt@gmail.com>
2021-03-02 13:25:46 -0800

    Enchant prismarine shard + rename

--- src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
+++ src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
@@ -3,17 +3,10 @@ package dev.arcticdevelopment.arctictools.controllers;
 import dev.arcticdevelopment.arctictools.ArcticTools;
 import dev.arcticdevelopment.arctictools.utilities.rods.FishDrop;
 import dev.kyro.arcticapi.misc.AOutput;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
-import java.awt.*;
 
 public class SellManager {
 
@@ -28,7 +21,6 @@ public class SellManager {
 		for(int i = 0 ; i < inventory.getSize() ; i++) {
 			ItemStack item = inventory.getItem(i);
 
-
 			if (item == null) {
 				continue;
 			}
@@ -38,11 +30,9 @@ public class SellManager {
 				 testItem = new ItemStack(item);
 				 testItem.setAmount(1);
 
-
 				if (fishDrop.getDrop().equals(testItem)) {
 					counter += item.getAmount();
 					total += fishDrop.getSellPrice() * item.getAmount();
-
 
 					inventory.setItem(i,new ItemStack(Material.AIR));
 				}
