58482536617b3e0c36729b2dfafa26955c9903a6
OX-S <finn.kliewer1@gmail.com>
2021-03-02 12:45:12 -0500

    Fish get removed on inventory on sell

--- src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
+++ src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
@@ -2,12 +2,18 @@ package dev.arcticdevelopment.arctictools.controllers;
 
 import dev.arcticdevelopment.arctictools.ArcticTools;
 import dev.arcticdevelopment.arctictools.utilities.rods.FishDrop;
+import dev.kyro.arcticapi.misc.AOutput;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.awt.*;
 
 public class SellManager {
 
@@ -17,8 +23,12 @@ public class SellManager {
 
 		Inventory inventory = player.getInventory();
 		ItemStack testItem;
+		int counter = 0;
 
-		for (ItemStack item: inventory.getContents()){
+		for(int i = 0 ; i < inventory.getSize() ; i++) {
+			ItemStack item = inventory.getItem(i);
+
+
 			if (item == null) {
 				continue;
 			}
@@ -30,11 +40,16 @@ public class SellManager {
 
 
 				if (fishDrop.getDrop().equals(testItem)) {
-
+					counter += item.getAmount();
 					total += fishDrop.getSellPrice() * item.getAmount();
+
+
+					inventory.setItem(i,new ItemStack(Material.AIR));
 				}
 			}
 		}
+		AOutput.send(player, "Sold &d" + counter + "&f fish for &d$" + total + "&f.");
+
 		System.out.println(total);
 		ArcticTools.VAULT.depositPlayer(player,total);
 	}
