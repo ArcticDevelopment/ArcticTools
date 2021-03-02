fbb8c0661aed03192a87e4db714b6f6c5595ac33
OX-S <finn.kliewer1@gmail.com>
2021-03-02 12:18:48 -0500

    Made /sellfish handle stacked items

--- src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
+++ src/main/java/dev/arcticdevelopment/arctictools/controllers/SellManager.java
@@ -4,6 +4,7 @@ import dev.arcticdevelopment.arctictools.ArcticTools;
 import dev.arcticdevelopment.arctictools.utilities.rods.FishDrop;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -15,14 +16,22 @@ public class SellManager {
 		int total = 0;
 
 		Inventory inventory = player.getInventory();
+		ItemStack testItem;
 
 		for (ItemStack item: inventory.getContents()){
+			if (item == null) {
+				continue;
+			}
 
 			for (FishDrop fishDrop: FishDrop.drops) {
 
-				if (fishDrop.getDrop().equals(item)) {
+				 testItem = new ItemStack(item);
+				 testItem.setAmount(1);
 
-					total += fishDrop.getSellPrice();
+
+				if (fishDrop.getDrop().equals(testItem)) {
+
+					total += fishDrop.getSellPrice() * item.getAmount();
 				}
 			}
 		}
